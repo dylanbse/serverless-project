c2d84508dad2a6fa142927de6d41d8c1673c7e4a
Dylan <dylanbotuli@hotmail.co.uk>
2022-04-20 15:24:58 +0100

    removed hardcoded value

--- src/utils/publishTopic.ts
+++ src/utils/publishTopic.ts
@@ -3,7 +3,7 @@ import SNS from 'aws-sdk/clients/sns'
 export const publishToTopic = async (jsonPayload: string, snsClient: SNS): Promise<any> => {
     const publishedMessage = await snsClient.publish({
       Message: JSON.stringify(jsonPayload),
-      TopicArn: "arn:aws:sns:eu-west-1:872030569168:dev-my-topic"
+      TopicArn: process.env.MAIN_TOPIC_ARN
     })
     .promise()
     return publishedMessage
